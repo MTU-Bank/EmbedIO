5d484d4020ba92f3b7fc2594d8aba5b539a53e54
kolya5544 <kolya@iktm.me>
2024-03-11 00:22:16 +0300

    Implemented UnignoreJson attribute to allow for override of default JsonIgnore attributes- ended up not using it. Either way, change might prove useful in the future (we still need a way to deliver the token to the service)

--- src/EmbedIO/GetOnlyJsonPropertyAttribute.cs
+++ src/EmbedIO/GetOnlyJsonPropertyAttribute.cs
@@ -12,16 +12,28 @@ namespace EmbedIO
     {
     }
 
+    [System.AttributeUsage(System.AttributeTargets.Property, AllowMultiple = false)]
+    public class UnignoreJson : Attribute
+    {
+    }
+
     public class GetOnlyContractResolver : DefaultContractResolver
     {
         protected override JsonProperty CreateProperty(MemberInfo member, MemberSerialization memberSerialization)
         {
             var property = base.CreateProperty(member, memberSerialization);
+           
             if (property != null && property.Writable)
             {
+                // check for GetOnlyJsonPropertyAttribute for deserialization
                 var attributes = property.AttributeProvider.GetAttributes(typeof(GetOnlyJsonPropertyAttribute), true);
                 if (attributes != null && attributes.Count > 0)
                     property.Writable = false;
+
+                // check for UnignoreJson for serialization
+                attributes = property.AttributeProvider.GetAttributes(typeof(UnignoreJson), true);
+                if (attributes != null && attributes.Count > 0)
+                    property.Readable = true;
             }
             return property;
         }
